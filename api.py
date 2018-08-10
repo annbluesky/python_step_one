c483d35837df5cdaf50fe0dd0e22a585c4bac29e
Annbluesky <ann.aknowan@gmail.com>
2018-08-10 08:43:34 +0700

    add route home

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ books = [
 ]
 
 
-@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
     return '''<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>'''
